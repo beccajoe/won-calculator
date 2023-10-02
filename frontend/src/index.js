c2ba49834815ec3fe23e77b2c81a3d5d99030d2f
Becca <123378408+beccajoe@users.noreply.github.com>
2023-10-02 19:11:57 +0900

    Upgrade to React 18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { LanguageProvider } from './context/LanguageContext';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
     <React.StrictMode>
         <LanguageProvider>
             <App />
         </LanguageProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Update usage of ReactDOM.unmountComponentAtNode
+const unmount = () => {
+    root.unmount();
+};
+
+export { unmount }; // Export the unmount function if needed
