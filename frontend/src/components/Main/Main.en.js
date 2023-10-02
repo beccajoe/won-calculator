22ae69f18c33e73a93f89d476a564b4c9630b9cc
Becca <123378408+beccajoe@users.noreply.github.com>
2023-10-02 14:37:52 +0900

    Add main page component

---
+++ frontend/src/components/Main/Main.en.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeaderEn from '../Header/Header.en';
+import Footer from '../Footer/Footer';
+import Calculator from '../Calculator/Calculator';
+
+function MainEn() {
+    return (
+        <div>
+            <HeaderEn />
+            <Calculator />
+            <Footer />
+        </div>
+    );
+}
+
+export default MainEn;
